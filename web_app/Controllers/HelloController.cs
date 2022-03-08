1b903b22fcea0040a1c12b8ff1eca7c18c4b17f2
Gage <gw_eveland@hotmail.com>
2022-03-08 10:19:27 -0600

    Add project files.

---
+++ web_app/Controllers/HelloController.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace web_app.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("<h1>Hello World!</h1>", "text/html");
+        }
+
+        public IActionResult Goodbye()
+        {
+            return Content("Goodbye");
+        }
+    }
+}
